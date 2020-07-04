1daab2b256c747542e59e5de146a3a60f3564a97
Olga Orlova <olga.orlova152@gmail.com>
2020-07-04 17:11:22 +0300

    badges improved

--- src/pages/plant/Badges.jsx
+++ src/pages/plant/Badges.jsx
@@ -1,44 +1,57 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Box, Avatar } from '@material-ui/core';
 import FilterVintageIcon from '@material-ui/icons/FilterVintage';
 import EcoIcon from '@material-ui/icons/Eco';
 import ScatterPlotIcon from '@material-ui/icons/ScatterPlot';
 import theme from '../../theme';
-
-const Badge = ({ children, backgroundColor = theme.palette.primary, ...rest }) => {
+const Badge = ({ children, title, backgroundColor = theme.palette.primary, ...rest }) => {
     const style = {
         fontSize: 'small',
         backgroundColor,
     };
 
     return (
-        <Box mr={1}>
+        <Box mr={1} title={title}>
             <Avatar {...rest} style={style}>{children}</Avatar>
         </Box>
     );
 }
 
 export default ({ plant }) => {
+    const { t } = useTranslation();
 
     return (
         <Box display="flex" p={2}>
             {!!plant.main_species.flower.color && (
-                <Badge backgroundColor={plant.main_species.flower.color}>
+                <Badge
+                    backgroundColor={plant.main_species.flower.color}
+                    title={t('plant_details.badges.flower_color', { color: plant.main_species.flower.color })}
+                >
                     <FilterVintageIcon />
                 </Badge>
             )}
             {!!plant.main_species.foliage.color && (
-                <Badge backgroundColor={plant.main_species.foliage.color}>
+                <Badge
+                    backgroundColor={plant.main_species.foliage.color}
+                    title={t('plant_details.badges.foliage_color', { color: plant.main_species.foliage.color })}
+                >
                     <EcoIcon />
                 </Badge>
             )}
             {!!plant.main_species.fruit_or_seed.color && (
-                <Badge backgroundColor={plant.main_species.fruit_or_seed.color}>
+                <Badge
+                    backgroundColor={plant.main_species.fruit_or_seed.color}
+                    title={t('plant_details.badges.seed_color', { color: plant.main_species.fruit_or_seed.color })}
+                >
                     <ScatterPlotIcon />
                 </Badge>
             )}
             {!!plant.main_species.growth.temperature_minimum.deg_c && (
-                <Badge backgroundColor="LightSkyBlue">
+                <Badge
+                    backgroundColor="LightSkyBlue"
+                    title={t('plant_details.badges.min_t', { t: `${Math.round(plant.main_species.growth.temperature_minimum.deg_c)}°C` })}
+                >
                     {`${Math.round(plant.main_species.growth.temperature_minimum.deg_c)}°C`}
                 </Badge>
             )}
